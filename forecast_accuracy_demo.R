89eb37c0086fdc5a61e995c13d3d8045d9b5efa6
slee <sanlee8495@gmail.com>
2022-11-08 19:05:23 -0800

    11/8/2022

--- forecast_accuracy_demo.R
+++ forecast_accuracy_demo.R
@@ -20,11 +20,15 @@ dsx[-1, ] -> dsx
 dsx %>% 
   janitor::clean_names() %>% 
   readr::type_convert() %>% 
-  data.frame()
+  data.frame() %>% 
+  dplyr::relocate(forecast_month_year_code, forecast_month_year_code, product_manufacturing_location_name, location_no, product_label_sku_code,
+                  product_label_sku_name, product_category_name, product_platform_name, product_group_code, product_manufacturing_line_area_no_code,
+                  abc_4_id, safety_stock_id, mto_mts_gross_requirements_calc_method_id, adjusted_forecast_pounds_lbs, adjusted_forecast_cases,
+                  stat_forecast_pounds_lbs, stat_forecast_cases, product_label_sku_code, mto_mts_gross_requirements_calc_method_id, primary_channel_id,
+                  sub_segment_id)
 
 
-# I'm currently defining columns. 
-# C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 23/Forecast Accuracy and Bias/Sample report for me/Forecast Accuracy by Ship-Loc_(Example for New Metrics).xlsx
+
 # There are number of duplicated columns, and some columns I have no idea. 
 # Need to discuss with Wilson before I move
 
