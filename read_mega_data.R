ce289bfef26dceadaa5a75a3e3a0a5a98d4c188c
slee <sanlee8495@gmail.com>
2022-11-22 11:39:32 -0800

    11/22/22

---
+++ read_mega_data.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(magrittr)
+library(openxlsx)
+library(readxl)
+library(writexl)
+library(reshape2)
+library(skimr)
+library(janitor)
+library(lubridate)
+
+mega_data <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 23/Forecast Accuracy and Bias/Mega Data/mega_data_upto_oct_2022.xlsx")
+
+959214
+
+# delete NA in the first column (date divider for Wilson)
+
+
+
+
